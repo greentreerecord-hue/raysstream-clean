d45db3b306a15066d352c1b4ae24157696dd36f4
greentreerecord-hue <greentreerecord@gmail.com>
2026-06-18 14:39:25 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,9 @@
-
+export default function Home() {
+  return (
+    <main>
+      <h1>Ray'sStream</h1>
+      <p>Welcome to the new Ray'sStream.</p>
+      <a href="/watch/itscool">Watch Video</a>
+    </main>
+  );
+}
