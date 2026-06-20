b2cb305c8cd29239be1cef313eef3b482f6a86af
greentreerecord-hue <greentreerecord@gmail.com>
2026-06-20 10:16:54 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,65 +2,21 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <main
-      style={{
-        background: "#050505",
-        color: "white",
-        minHeight: "100vh",
-        padding: "24px",
-      }}
-    >
-      <h1
-        style={{
-          color: "#ff6a00",
-          fontSize: "48px",
-        }}
-      >
-        🔥 Ray'sStream
-      </h1>
-
+    <main style={{ background: "#050505", color: "white", minHeight: "100vh", padding: 24 }}>
+      <h1 style={{ color: "#ff6a00", fontSize: 48 }}>🔥 Ray'sStream</h1>
       <p>Home Video Library</p>
 
-      <div style={{ marginTop: "30px" }}>
-        <Link
-          href="/watch/itscool"
-          style={{
-            display: "block",
-            color: "white",
-            marginBottom: "20px",
-            textDecoration: "none",
-            fontSize: "32px",
-          }}
-        >
-          It's Cool
-        </Link>
+      <Link href="/watch/itscool" style={{ display: "block", color: "white", fontSize: 32, marginTop: 30 }}>
+        It's Cool
+      </Link>
 
-        <Link
-          href="/watch/video2"
-          style={{
-            display: "block",
-            color: "white",
-            marginBottom: "20px",
-            textDecoration: "none",
-            fontSize: "32px",
-          }}
-        >
-          Video 2
-        </Link>
+      <Link href="/watch/video2" style={{ display: "block", color: "white", fontSize: 32, marginTop: 20 }}>
+        Video 2
+      </Link>
 
-        <Link
-          href="/watch/video3"
-          style={{
-            display: "block",
-            color: "white",
-            marginBottom: "20px",
-            textDecoration: "none",
-            fontSize: "32px",
-          }}
-        >
-          Spaceship
-        </Link>
-      </div>
+      <Link href="/watch/video3" style={{ display: "block", color: "white", fontSize: 32, marginTop: 20 }}>
+        Spaceship
+      </Link>
     </main>
   );
 } 
