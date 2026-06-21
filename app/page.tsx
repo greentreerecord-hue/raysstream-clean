e86dbcb7cbe07a25477ca18cb3a5941d2d93db55
greentreerecord-hue <greentreerecord@gmail.com>
2026-06-21 10:28:38 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
-mport Link from "next/link";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main style={{ padding: 20 }}>
       <h1>Ray'sStream</h1>
 
-      <ul>
-        <li><Link href="/watch/itscool">It's Cool</Link></li>
-        <li><Link href="/watch/video2">Video 2</Link></li>
-        <li><Link href="/watch/video3">Video 3</Link></li>
-      </ul>
+      <p>
+        <Link href="/watch/itscool">It's Cool</Link>
+      </p>
+
+      <p>
+        <Link href="/watch/video2">Video 2</Link>
+      </p>
+
+      <p>
+        <Link href="/watch/video3">Spaceship</Link>
+      </p>
     </main>
   );
 } 
-
